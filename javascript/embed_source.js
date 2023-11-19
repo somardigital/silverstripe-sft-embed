16658b3b24ab98daed486a6def979f87ff13ba1e
Normann Lou <normann@somar.co.nz>
2023-11-20 11:38:17 +1300

    FVHP-281 Make the source code human readable and re-minify embed.js
    Update README file

--- javascript/embed_source.js
+++ javascript/embed_source.js
@@ -1,51 +1,101 @@
 (function () {
-    const e = "Home - Service Finder",
-        t = "https://d2mr1k8kix77a1.cloudfront.net/",
-        o = "service-finder-tool";
-    let n;
-    (window.openServiceFinderTool = (i) => {
-        i && ((n = i.target), i.preventDefault());
-        const l = document.getElementById(o);
-        if (l instanceof Element) return void l.focus();
-        const d = document.createElement("div");
-        (d.id = o), (d.tabIndex = "-1"), (d.style.position = "fixed"), (d.style.top = 0), (d.style.left = 0), (d.style.right = 0), (d.style.bottom = 0), (d.style.zIndex = 9999);
-        const r = document.createElement("div");
-        (r.style.background = "rgba(0, 0, 0, 0.6)"), (r.style.position = "absolute"), (r.style.top = 0), (r.style.left = 0), (r.style.right = 0), (r.style.bottom = 0);
-        const s = document.createElement("iframe");
-        s.setAttribute("src", t),
-            s.setAttribute("title", e),
-            (s.style.border = "none"),
-            (s.style.width = "90%"),
-            (s.style.height = "90%"),
-            (s.style.position = "absolute"),
-            (s.style.top = "5%"),
-            (s.style.left = "5%"),
-            r.addEventListener("click", window.closeServiceFinderTool),
-            d.addEventListener("keydown", (e) => {
-                "Escape" === e.key && window.closeServiceFinderTool();
-            }),
-            d.appendChild(r),
-            r.appendChild(s),
-            document.querySelector("body").appendChild(d),
-            (document.querySelector("body").style.overflow = "hidden"),
-            d.focus();
-    }),
-        (window.closeServiceFinderTool = () => {
-            document.querySelector("body").style.overflow = "";
-            const e = document.getElementById(o);
-            e instanceof Element && (e.remove(), n instanceof Element && n.focus());
-        }),
-        (window.initServiceFinderTool = (e = ".js-service-finder-tool-trigger") => {
-            const t = document.querySelectorAll(e);
-            t.length &&
-            t.forEach((e) => {
-                e.addEventListener("click", window.openServiceFinderTool);
-            });
-        }),
-        document.addEventListener("DOMContentLoaded", () => {
-            window.initServiceFinderTool();
-        }),
-        window.addEventListener("message", (e) => {
-            "closeServiceFinderTool" === e.data && window.closeServiceFinderTool();
+    const serviceFinderToolTitle = "Home - Service Finder";
+    const serviceFinderToolUrl = "https://d2mr1k8kix77a1.cloudfront.net/";
+    const wrapperElementId = "service-finder-tool";
+    let openTriggerElement;
+
+    (window.openServiceFinderTool = (openEvent) => {
+        if (openEvent) {
+            openTriggerElement = openEvent.target;
+            openEvent.preventDefault();
+        }
+
+        // If the Service Finder Tool is already open then focus on the existing tool rather than create a new one
+        const existingWrapperEl = document.getElementById(wrapperElementId);
+        if (existingWrapperEl instanceof Element) {
+            existingWrapperEl.focus();
+            return;
+        }
+
+        const wrapperElement = document.createElement('div');
+        wrapperElement.id = wrapperElementId;
+        wrapperElement.tabIndex = '-1';
+        wrapperElement.style.position = 'fixed';
+        wrapperElement.style.top = 0;
+        wrapperElement.style.left = 0;
+        wrapperElement.style.right = 0;
+        wrapperElement.style.bottom = 0;
+        wrapperElement.style.zIndex = 9999;
+
+        const overlayElement = document.createElement('div');
+        overlayElement.style.background = 'rgba(0, 0, 0, 0.6)';
+        overlayElement.style.position = 'absolute';
+        overlayElement.style.top = 0;
+        overlayElement.style.left = 0;
+        overlayElement.style.right = 0;
+        overlayElement.style.bottom = 0;
+
+        const frameElement = document.createElement('iframe');
+        frameElement.setAttribute('src', serviceFinderToolUrl);
+        frameElement.setAttribute('title', serviceFinderToolTitle);
+        frameElement.style.border = 'none';
+        frameElement.style.width = '90%';
+        frameElement.style.height = '90%';
+        frameElement.style.position = 'absolute';
+        frameElement.style.top = '5%';
+        frameElement.style.left = '5%';
+
+        overlayElement.addEventListener('click', window.closeServiceFinderTool);
+        wrapperElement.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') {
+                window.closeServiceFinderTool();
+            }
         });
+
+        wrapperElement.appendChild(overlayElement);
+        overlayElement.appendChild(frameElement);
+
+        document.querySelector('body').appendChild(wrapperElement);
+        document.querySelector('body').style.overflow = 'hidden';
+
+        wrapperElement.focus();
+    }),
+
+    (window.closeServiceFinderTool = () => {
+        document.querySelector('body').style.overflow = '';
+
+        const wrapperElement = document.getElementById(wrapperElementId);
+
+        if (wrapperElement instanceof Element) {
+            wrapperElement.remove();
+
+            // Focus back on the element which opened the tool to avoid the user's focus being at the end of the body tag
+            if (openTriggerElement instanceof Element) {
+                openTriggerElement.focus();
+            }
+        }
+    }),
+
+    (window.initServiceFinderTool = (selector = ".js-service-finder-tool-trigger") => {
+        const triggerElements = document.querySelectorAll(selector);
+
+        // Breakout if no elements are found
+        if (!triggerElements.length) {
+            return;
+        }
+
+        triggerElements.forEach((el) => {
+            el.addEventListener('click', window.openServiceFinderTool);
+        });
+    }),
+
+    document.addEventListener("DOMContentLoaded", () => {
+        window.initServiceFinderTool();
+    }),
+
+    window.addEventListener("message", (event) => {
+        if (event.data === 'closeServiceFinderTool') {
+            window.closeServiceFinderTool();
+        }
+    });
 })();
